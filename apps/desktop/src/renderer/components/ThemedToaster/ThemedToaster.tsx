fbe8ca79cfeb774e64336079acec9d207b9ee3b3
Josh Berg <joshberg24@gmail.com>
2026-09-02 08:48:26 -0500

    ✨ add close button to dismissable toasts

--- apps/desktop/src/renderer/components/ThemedToaster/ThemedToaster.tsx
+++ apps/desktop/src/renderer/components/ThemedToaster/ThemedToaster.tsx
@@ -3,5 +3,5 @@ import { useTheme } from "renderer/stores/theme/store";
 
 export function ThemedToaster() {
 	const theme = useTheme();
-	return <Toaster expand theme={theme?.type ?? "dark"} />;
+	return <Toaster expand closeButton theme={theme?.type ?? "dark"} />;
 }
